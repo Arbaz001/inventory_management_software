1dfd29ba9aa6135f5772940b4f9ab6119d304fcf
Arbaz001 <arbazalisgl@gmail.com>
2025-06-01 11:32:17 +0530

    #FIX db Url

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/inventory";
+const MONGO_URI = process.env.DATABASE_URL || "mongodb://mongo:VfDNQuBVwvVoXogtAurjpyoXTMpeseTR@turntable.proxy.rlwy.net:30038";
 
 mongoose.connect(MONGO_URI, {
   // useNewUrlParser: true, // Not needed in mongoose 6+
